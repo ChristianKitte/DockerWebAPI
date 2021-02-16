94491b21b8ff7619918463c18eed2e23d7c35549
Christian Kitte <christian.kitte@stud.hs-emden-leer.de>
2021-02-16 16:31:57 +0100

    Projektdateien hinzufügen.

---
+++ DBServer/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DBServer
+{
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+        [Required]
+        public int Vorname { get; set; }
+        [Required]
+        public int Nachname { get; set; }
+    }
+}
---
+++ DBServer/PersonenContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBServer
+{
+    public class PersonenContext :DbContext
+    {
+        public DbSet<Person>Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite("Data-Source=personen.db");
+        }
+    }
+}
