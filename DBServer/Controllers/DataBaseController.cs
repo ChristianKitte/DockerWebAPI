957902074a1afb3cdb1de02e9ab58e69e0185370
Christian Kitte <christian.kitte@stud.hs-emden-leer.de>
2021-02-17 16:24:43 +0100

    Restliche Routen für REST implementiert und Docker vorbereitet

--- DBServer/Controllers/DataBaseController.cs
+++ DBServer/Controllers/DataBaseController.cs
@@ -20,19 +20,53 @@ namespace DBServer.Controllers
             _context = context;
         }
 
-        [HttpGet("api/v1/[controller]")]
+        [HttpGet("api/v1/get_persons")]
         public IActionResult Get()
         {
             var persons = _context.Persons.Select(x => x).ToArray();
             return Ok(persons);
         }
 
-        [HttpGet]
-        [HttpGet("api/v1/[controller]{id}")]
+        [HttpGet("api/v1/get_person_by_id/{id}")]
         public IActionResult Get(int id)
         {
-            var persons = _context.Persons.Where(x => x.PersonID == id).FirstOrDefault();
+            var persons = _context.Persons.FirstOrDefault(x => x.PersonID == id);
             return Ok(persons);
+        }
+        
+        [HttpPut("api/v1/add_person/{vorname}/{nachname}")]
+        public IActionResult Put(string vorname, string nachname)
+        {
+            var person = new Person();
+            person.Vorname = vorname;
+            person.Nachname = nachname;
+
+            _context.Persons.Add(person);
+            _context.SaveChanges();
+
+            return Ok(person);
+        }
+        
+        [HttpPost("api/v1/update_person/{id}/{vorname}/{nachname}")]
+        public IActionResult Post(int id,string vorname, string nachname)
+        {
+            var person = _context.Persons.FirstOrDefault(x=>x.PersonID==id);
+            person.Vorname = vorname;
+            person.Nachname = nachname;
+
+            _context.SaveChanges();
+
+            return Ok(person);
+        }
+
+        [HttpDelete("api/v1/delete_person/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var person = _context.Persons.Where(x => x.PersonID == id).FirstOrDefault();
+            _context.Persons.Remove(person);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
