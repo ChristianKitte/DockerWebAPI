cb228c004f0db3e275e4e1d37f2450c014278ae1
Christian Kitte <christian.kitte@stud.hs-emden-leer.de>
2021-02-18 16:20:57 +0100

    Modifikationen am Controller und Dockerfile. Unterprojekt DBClient entfernt.

--- DBServer/Controllers/PersonsController.cs
+++ DBServer/Controllers/PersonsController.cs
@@ -21,59 +21,103 @@ namespace DBServer.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public IActionResult GetAll()
         {
             try
             {
                 var person = _context.Persons.Select(x => x).ToArray();
+                HttpContext.Response.StatusCode = StatusCodes.Status200OK;
                 return Ok(person.ToList());
             }
             catch (Exception f)
             {
-                return Ok(f);
+                HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return Forbid();
             }
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public IActionResult GetByID(int id)
         {
-           var persons = _context.Persons.FirstOrDefault(x => x.PersonID == id);
-           return Ok(persons);
+            try
+            {
+                var persons = _context.Persons.FirstOrDefault(x => x.PersonID == id);
+                return Ok(persons);
+            }
+            catch (Exception f)
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return Forbid();
+            }
         }
-        
+
         [HttpPut("{vorname}/{nachname}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public IActionResult AddPerson(string vorname, string nachname)
         {
-            var person = new Person();
-            person.Vorname = vorname;
-            person.Nachname = nachname;
+            try
+            {
+                var person = new Person();
+                person.Vorname = vorname;
+                person.Nachname = nachname;
 
-            _context.Persons.Add(person);
-            _context.SaveChanges();
+                _context.Persons.Add(person);
+                _context.SaveChanges();
 
-            return Ok(person);
+                return Ok(person);
+            }
+            catch (Exception f)
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return Forbid();
+            }
         }
-        
+
         [HttpPost("{id}/{vorname}/{nachname}")]
-        public IActionResult UpdatePerson(int id,string vorname, string nachname)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public IActionResult UpdatePerson(int id, string vorname, string nachname)
         {
-            var person = _context.Persons.FirstOrDefault(x=>x.PersonID==id);
-            person.Vorname = vorname;
-            person.Nachname = nachname;
+            try
+            {
+                var person = _context.Persons.FirstOrDefault(x => x.PersonID == id);
+                person.Vorname = vorname;
+                person.Nachname = nachname;
 
-            _context.SaveChanges();
+                _context.SaveChanges();
 
-            return Ok(person);
+                return Ok(person);
+            }
+            catch (Exception f)
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return Forbid();
+            }
         }
 
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         public IActionResult DeletePerson(int id)
         {
-            var person = _context.Persons.Where(x => x.PersonID == id).FirstOrDefault();
-            _context.Persons.Remove(person);
-            _context.SaveChanges();
+            try
+            {
+                var person = _context.Persons.Where(x => x.PersonID == id).FirstOrDefault();
+                _context.Persons.Remove(person);
+                _context.SaveChanges();
 
-            return NoContent();
+                return NoContent();
+            }
+            catch (Exception f)
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return Forbid();
+            }
         }
     }
 }
